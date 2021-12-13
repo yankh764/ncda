96643a596df02ec71fbd3f849a54a22df3d9b7fa
yankh764 <yankh18@protonmail.com>
2021-12-13 23:11:14 +0200

    Almost finished the initial display

---
+++ include/const_params.h
@@ -0,0 +1,21 @@
+/*
+---------------------------------------------------------
+| License: GNU GPL-3.0                                  |
+---------------------------------------------------------
+| This header file contains all the necessary constant  |
+| parameters and settings for the ncurses session       |
+---------------------------------------------------------
+*/
+#ifndef _CONST_PARAMS_H
+#define _CONST_PARAMS_H
+
+#include <ncurses.h>
+
+const int _def_attrs = A_BOLD;
+const char _separator = '|';
+const int _blank = 1;
+const int _max_print_fsize = 8;
+const int _max_print_mtime = 11;
+const int _fname_x = 26;
+
+#endif
--- src/curses.c
+++ src/curses.c
@@ -11,7 +11,6 @@
  * Defining _GNU_SOURCE macro since it achives all the desired
  * feature test macro requirements, which are:
  *     1) _DEFAULT_SOURCE || _BSD_SOURCE for file type and mode macros
- *     2) _POSIX_C_SOURCE || _BSD_SOURCE for ctime_r()
  */
 #define _GNU_SOURCE
 #include <string.h>
@@ -19,6 +18,7 @@
 #include <stdbool.h>
 #include "ncda.h"
 #include "disk_man.h"
+#include "const_params.h"
 #include "curses.h"
 
 /*
@@ -50,8 +50,6 @@ enum COLOR_PAIRS_NUM {
 };
 
 const struct doubly_list *_highligted_node; 
-const int _def_attrs = A_BOLD;
-const int _fname_x = 26;
 
 
 static short get_color_pair_num(mode_t st_mode)
@@ -109,36 +107,40 @@ void nc_get_cdata_fields(struct doubly_list *head)
 
 static int print_separator(WINDOW *wp, int y, int x)
 {
-	const char separator = '|';
 	int curx;
 
-	if (mvwprintw(wp, y, x, "%c", separator) == ERR)
+	if (mvwprintw(wp, y, x, "%c", _separator) == ERR)
 		return -1;
 	if ((curx = getcurx(wp)) == ERR)
 		return -1;
 	return curx;
 }
 
-static inline int dye_fsize_size(WINDOW *wp, int y, int x, int len)
+static inline int dye_val(WINDOW *wp, int y, int x, int len)
 {
 	const short cpair = YELLOW_PAIR;
 
 	return (mvwchgat(wp, y, x, len, _def_attrs, cpair, NULL) == ERR) ? -1 : 0;
 }
 
-static inline int dye_fsize_unit(WINDOW *wp, int y, int x, int len)
+static inline int dye_unit(WINDOW *wp, int y, int x, int len)
 {
 	const short cpair = RED_PAIR;
 
 	return (mvwchgat(wp, y, x, len, _def_attrs, cpair, NULL) == ERR) ? -1 : 0;
 }
 
+static inline int dye_fsize(WINDOW *wp, int y, int x)
+{
+	const int max_size_len = 5;
+	const int max_unit_len = 2;
+
+	return (dye_val(wp, y, x, max_size_len) || 
+		dye_unit(wp, y, x+max_size_len+_blank, max_unit_len)) ? -1 : 0;
+}
+
 static int print_fsize(WINDOW *wp, int y, int x, off_t bytes)
 {
-	const int max_print_len = 8;
-	const int max_size_digits = 5;
-	const int max_unit_len = 2;
-	const int blank_s = 1;
 	struct size_format format;
 	int curx;
 
@@ -149,36 +151,42 @@ static int print_fsize(WINDOW *wp, int y, int x, off_t bytes)
 	if ((curx = getcurx(wp)) == ERR)
 		return -1;
 	if (COLORED_OUTPUT)
-		if (dye_fsize_size(wp, y, x, curx-x-max_unit_len) || 
-		    dye_fsize_unit(wp, y, x+max_size_digits+blank_s, curx-x-max_size_digits))
+		if (dye_fsize(wp, y, x))
 			return -1;
-	return print_separator(wp, y, x+max_print_len+blank_s);
+	return print_separator(wp, y, x+_max_print_fsize+_blank);
 }
 
-static inline int dye_mtime(WINDOW *wp, int y, int x, int len)
+static inline int dye_mtime(WINDOW *wp, int y, int x)
 {
-	const short cpair = YELLOW_PAIR;
+	const int max_priod_len = 9;
+	const int max_num_len = 1;
 
-	return (mvwchgat(wp, y, x, len, _def_attrs, cpair, NULL) == ERR) ? -1 : 0;
+	return (dye_val(wp, y, x, max_num_len) || 
+		dye_unit(wp, y, x+max_num_len+_blank, max_priod_len)) ? -1 : 0;
 }
 
 static int print_mtime(WINDOW *wp, int y, int x, time_t mtime)
 {
-	const int full_len = 11;
-	const int blank_s = 1;
 	char *buf;
 	int curx;
 
 	if(!(buf = get_mtime_str(mtime)))
-		;//goto err_out;
+		return -1;
 	if (mvwprintw(wp, y, x, "%s", buf) == ERR)
-		;//goto err_free_buf;
+		goto err_free_buf;
 	if ((curx = getcurx(wp)) == ERR)
-		;//goto err_free_buf;
+		goto err_free_buf;
 	if (COLORED_OUTPUT)
-		if (dye_mtime(wp, y, curx, curx-x))
-			;//goto err_free_buf;
-	return print_separator(wp, y, x+full_len+blank_s);
+		if (dye_mtime(wp, y, x))
+			goto err_free_buf;
+	free(buf);
+
+	return print_separator(wp, y, x+_max_print_mtime+_blank);
+
+err_free_buf:
+	free(buf);
+	
+	return -1;
 }
 
 static inline int dye_fname(WINDOW *wp, int y, int len, short cpair)
@@ -201,6 +209,18 @@ static int print_fname(WINDOW *wp, int y, const char *name, char eos, short cpai
 	return curx;
 }
 
+static int print_separators_only(WINDOW *wp, int y, int begin_x)
+{
+	int x1; 
+	int x2;
+
+	x1 = begin_x + _max_print_fsize + _blank;
+	x2 = x1 + sizeof(_separator) + _blank + _max_print_mtime + _blank;
+
+	return (print_separator(wp, y, x1) == -1 || 
+		print_separator(wp, y, x2) == -1) ? -1 : 0;
+}
+
 static inline int display_entries_info(WINDOW *wp,
 				       const struct doubly_list *node)
 {
@@ -210,19 +230,19 @@ static inline int display_entries_info(WINDOW *wp,
 	const char *name = node->data->file_data->fname;
 	const char eos = node->data->curses_data->eos;
 	const int y = node->data->curses_data->y;
-	const int blank_s = 1;
 	const int begin_x = 0;
 	int curx;
 
-	if (!is_dot_entry(name)) {
+	if (is_dot_entry(name)) {
+		if (print_separators_only(wp, y, begin_x))
+			return -1;
+	} else {
 		if ((curx = print_fsize(wp, y, begin_x, fsize)) == -1)
 			return -1;
-		if ((curx = print_mtime(wp, y, curx+blank_s, mtime)) == -1)
+		if ((curx = print_mtime(wp, y, curx+_blank, mtime)) == -1)
 			return -1;
 	}
-	if ((curx = print_fname(wp, y, name, eos, color_pair)) == -1)
-		return -1;
-	return 0;
+	return (print_fname(wp, y, name, eos, color_pair) == -1) ? -1 : 0;
 }
 
 int nc_display_entries(WINDOW *wp, const struct doubly_list *head) 
@@ -275,7 +295,7 @@ static int init_local_setup(WINDOW *wp)
  */
 static int invisibilize_cursor()
 {
-	return curs_set(0) == ERR ? -1 : 0;
+	return (curs_set(0) == ERR) ? -1 : 0;
 }
 
 int nc_init_setup()
@@ -286,92 +306,128 @@ int nc_init_setup()
 		init_local_setup(stdscr)) ? -1 : 0;
 }
 
-static int print_opening_message(WINDOW *wp)
+static int display_opening_message(WINDOW *wp)
 {
 	const char *message = "Ncurses disk analyzer --- Press ? for help";
-	short cpair = COLORED_OUTPUT ? CYAN_PAIR : 0;
+	const int attrs = A_REVERSE;
+	const int begin_y = 0;
+	const int begin_x = 0;
+	short cpair;
 
-	return (mvwprintw(wp, 0, 0, "%s", message) == ERR || 
-	        mvwchgat(wp, 0, 0, -1, A_REVERSE, cpair, NULL) == ERR) ? -1 : 0;
+	cpair = COLORED_OUTPUT ? CYAN_PAIR : 0;
+
+	if (mvwprintw(wp, begin_y, begin_x, "%s", message) == ERR)
+		return -1;
+	else
+		return (mvwchgat(wp, begin_y, begin_x, 
+				 -1, attrs, cpair, NULL) == ERR) ? -1 : 0;
 }
 
-static int print_path(WINDOW *wp, const char *path, int y)
+static int print_path_summary(WINDOW *wp, 
+			      const char *path, 
+			      int max_y, int begin_x)
 {
-	return (mvwprintw(wp, y, 0, " Path: %s", path) == ERR) ? -1 : 0;
+	return (mvwprintw(wp, max_y, begin_x, "Path: %s", path) == ERR) ? -1 : 0;
 }
 
-static int print_usage(WINDOW *wp, const struct doubly_list *head, int y, int x)
+static int print_usage_summary(WINDOW *wp, 
+			       const struct doubly_list *head, 
+			       int max_y, int max_x)
 {
-	const char *message = "Total Disk Usage: ";
+	const char *message = "Total Disk Usage:";
 	struct size_format format;
 	size_t len;
 
-	/* 6 is the format.size, 1 is the space, 2 is format.unit */
-	len = strlen(message) + 6 + 1 + 2;
 	format = get_proper_size_format(get_total_disk_usage(head));
+	/* The 1 is because I added another digit after the floating point */
+	len = strlen(message) + _blank + _max_print_fsize + 1;
 
-	return (mvwprintw(wp, y, x-len, "%s%0.2f %s", 
-		message, format.size, format.unit) == ERR) ? -1 : 0;
+	return (mvwprintw(wp, max_y, max_x-len, "%s %0.2f %s", 
+			  message, format.size, format.unit) == ERR) ? -1 : 0;
 }
 
 static int summary_message(WINDOW *wp, 
 			   const struct doubly_list *head,
-			   const char *path, int y, int x)
+			   const char *path, int max_y, int max_x)
 {
-	return (print_path(wp, path, y) || print_usage(wp, head, y, x));
+	const int begin_x = 1;
+
+	return (print_path_summary(wp, path, max_y, begin_x) || 
+		print_usage_summary(wp, head, max_y, max_x)) ? -1 : 0;
 }
 
-static int print_summary_message(WINDOW *wp, 
-				 const struct doubly_list *head, 
-				 const char *current_path)
+static int display_summary_message(WINDOW *wp, 
+		   		   const struct doubly_list *head, 
+				   const char *current_path)
 {
-	short cpair = COLORED_OUTPUT ? CYAN_PAIR : 0;
-	int y, x;
+	const int attrs = A_REVERSE | A_BOLD;
+	const int begin_x = 0;
+	int max_y, max_x;
+	short cpair;
 
-	getmaxyx(wp, y, x);
-	
-	return (summary_message(wp, head, current_path, --y, --x) ||
-		mvwchgat(wp, y, 0, -1, 
-			 A_REVERSE | A_BOLD, 
-			 cpair, NULL) == ERR) ? -1 : 0;
+	cpair = COLORED_OUTPUT ? CYAN_PAIR : 0;
+	getmaxyx(wp, max_y, max_x);
+
+	if (summary_message(wp, head, current_path, --max_y, --max_x))
+		return -1;
+	else 
+		return (mvwchgat(wp, max_y, begin_x, 
+				 -1, attrs, cpair, NULL) == ERR) ? -1 : 0;
 }
 
 static int create_borders(WINDOW *wp)
 {
-	int mx, my;
+	const char border = '-';
+	const int begin_y = 1;
+	const int begin_x = 0;
+	int max_x, max_y;
 	
-	getmaxyx(wp, my, mx);
+	getmaxyx(wp, max_y, max_x);
+	
+	max_y = max_y - 2;
 
-	return (mvwhline(wp, 1, 0, '-', mx) == ERR || 
-		mvwhline(wp, my-2, 0, '-', mx) == ERR) ? -1 : 0; 
+	return (mvwhline(wp, begin_y, begin_x, border, max_x) == ERR || 
+		mvwhline(wp, max_y, begin_x, border, max_x) == ERR) ? -1 : 0; 
 }
 
-/*
- * This function is for the sake of readability
-*/
+static int restore_entry_colors(WINDOW *wp, int y, int begin_x, short cpair)
+{
+	int x1, x2;
+
+	x1 = begin_x;
+	x2 = x1 + _max_print_fsize + _blank + sizeof(_separator) + _blank;
+
+	return (dye_fsize(wp, y, x1) || 
+		dye_mtime(wp, y, x2) ||
+		dye_fname(wp, y, -1, cpair)) ? -1 : 0;
+}
+
 static inline int restore_prev_entry_design(WINDOW *wp)
 {
 	const short cpair = _highligted_node->prev->data->curses_data->cpair;
-	const char *name = _highligted_node->prev->data->file_data->fname;
 	const int y = _highligted_node->prev->data->curses_data->y;
-	const int len = strlen(name) + 1;
+	const int begin_x = 0;
 	
-	return (mvwchgat(wp, y, 0, -1, A_BOLD, 0, NULL) == ERR || 
-		dye_fname(wp, y, len, cpair)) ? -1 : 0;
+	if (mvwchgat(wp, y, begin_x, -1, _def_attrs, 0, NULL) == ERR)
+		return -1;
+	if (COLORED_OUTPUT)
+		return restore_entry_colors(wp, y, begin_x, cpair);
+	else 
+		return 0;
 }
 
-/*
- * This function is for the sake of readability
- */
 static inline int restore_next_entry_design(WINDOW *wp)
 {
 	const short cpair = _highligted_node->next->data->curses_data->cpair;
-	const char *name = _highligted_node->next->data->file_data->fname;
 	const int y = _highligted_node->next->data->curses_data->y;
-	const int len = strlen(name) + 1;
+	const int begin_x = 0;
 	
-	return (mvwchgat(wp, y, 0, -1, A_BOLD, 0, NULL) == ERR || 
-		dye_fname(wp, y, len, cpair)) ? -1 : 0;
+	if (mvwchgat(wp, y, begin_x, -1, _def_attrs, 0, NULL) == ERR)
+		return -1;
+	if (COLORED_OUTPUT)
+		return restore_entry_colors(wp, y, begin_x, cpair);
+	else 
+		return 0;
 }
 
 static inline int restore_entry_design(WINDOW *wp, int key)
@@ -386,10 +442,14 @@ static inline int restore_entry_design(WINDOW *wp, int key)
 
 static inline int update_highlight_loc(WINDOW *wp, int key)
 {
-	int y = _highligted_node->data->curses_data->y;
+	const int y = _highligted_node->data->curses_data->y;
+	const int attrs = _def_attrs | A_REVERSE;
+	const int begin_x = 0;
 
-	return (mvwchgat(wp, y, 0, -1, A_REVERSE | A_BOLD, 0, NULL) == ERR ||
-	        restore_entry_design(wp, key)) ? -1 : 0;
+	if (mvwchgat(wp, y, begin_x, -1, attrs, 0, NULL) == ERR)
+		return -1;
+	else
+		return restore_entry_design(wp, key);
 }
 
 static int init_highlight(WINDOW *wp, const struct doubly_list *head)
@@ -406,9 +466,9 @@ int nc_initial_display(WINDOW *wp,
 		       const struct doubly_list *head,
 		       const char *current_path)
 {
-	return (print_opening_message(wp) || 
+	return (display_opening_message(wp) || 
 		create_borders(wp) || nc_display_entries(wp, head) || 
-		print_summary_message(wp, head, current_path) ||
+		display_summary_message(wp, head, current_path) ||
 		init_highlight(wp, head) || wrefresh(wp) == ERR) ? -1 : 0;
 }
 
